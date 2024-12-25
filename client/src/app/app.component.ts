9bf7b655d39f9262d1c81db1cd6a6ee1d3ba738f
Jabrane Ben Hadj Messaoud <jabrane.benhadjmessaoud@gmail.com>
2024-12-25 09:47:39 +0100

    changes pulling

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+ import { MatIconModule } from '@angular/material/icon';
+ import { MatToolbarModule } from '@angular/material/toolbar';
+ import { MatCardModule } from '@angular/material/card';
+ import { MatSidenavModule } from '@angular/material/sidenav';
+ import { MatListModule } from '@angular/material/list';
+ import { MatTableModule } from '@angular/material/table';
+ import { MatInputModule } from '@angular/material/input';
+ import { MatFormFieldModule } from '@angular/material/form-field';
+ import { CommonModule } from '@angular/common';
 
+ 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule,MatButtonModule,MatIconModule,MatToolbarModule,MatCardModule,MatSidenavModule,MatListModule,MatTableModule,MatInputModule,MatFormFieldModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
